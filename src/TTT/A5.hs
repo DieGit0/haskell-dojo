65d0f6f4529a3d7b268ff0458e1e66a140552d37
Diego Matos <dgmatos30@gmail.com>
2023-11-20 16:32:48 +0000

    A5 #01-10: add file

--- src/TTT/A5.hs
+++ src/TTT/A5.hs
@@ -7,47 +7,103 @@ import TTT.A2
 import TTT.A3
 import TTT.A4
 
+newLine = putStrLn []
+
 -- Q#01
 
-printBoard = undefined
+printBoard :: Board -> IO ()
+printBoard = putStr . formatBoard
 
 -- Q#02
 _LOGO_PATH_ :: FilePath
 _LOGO_PATH_ = "./assets/ttt-logo.txt"
 
-printLogo = undefined
+printLogo :: IO ()
+printLogo = putStrLn =<< readFile _LOGO_PATH_
 
 -- Q#03
 _RANDOM_BOOL_ :: IO Bool
 _RANDOM_BOOL_ = uniformM globalStdGen
 
-firstPlayer = undefined
+firstPlayer :: IO Player
+firstPlayer = _RANDOM_BOOL_ >>= \bool -> return $ getFirstPlayer bool
+
+firstPlayer' :: IO ()
+firstPlayer' = do 
+                bool <- _RANDOM_BOOL_
+                putStrLn . showSquare $ getFirstPlayer bool
 
 -- Q#04
 
-getMove = undefined
+getMove :: Board -> IO Move
+getMove board = do 
+    strMove <- getLine
+    let move = stringToMove strMove
+    if isValidMove board move 
+        then  return move
+        else  putStrLn "Invalid move! Try again" >> getMove board
 
 -- Q#05
 
-play = undefined
+play :: Board -> IO ()
+play board = do 
+            when _DISPLAY_LOGO_ printLogo
+            newLine
+            printBoard board
+            player <- firstPlayer
+            loop player board
+    where   loop :: Player -> Board -> IO ()
+            loop p b = do
+                 newLine
+                 putStrLn $ promptPlayer p
+                 move     <-getMove      b
+                 let (state,newBoard) = playMove p b move
+                 checkState state newBoard
+                 where checkState st nb 
+                        | st == Prg = do
+                            putStrLn $ showGameState st
+                            printBoard nb 
+                            let nP = switchPlayer p
+                            loop nP nb
+                        | otherwise = do 
+                            putStrLn $ showGameState st
+                            printBoard nb
+                        -- if st == Prg 
+                        -- then 
+                        --     do
+                        --     printBoard nb 
+                        --     let newP = switchPlayer p
+                        --     loop newP  nb
+                        -- else 
+                        --     do 
+                        --     print st
+                        --     printBoard nb
 
 -- Q#06
 
 runTTT :: IO ()
-runTTT = putStrLn "Not implemented... yet!"
+runTTT = play _EMPTY_BOARD_
 
 -- Q#07
 
-printLogoDo = undefined
+printLogoDo :: IO ()
+printLogoDo = do
+    logo <- readFile _LOGO_PATH_
+    putStrLn logo 
 
 -- Q#08
 
-firstPlayerDo = undefined
+firstPlayerDo :: IO Player
+firstPlayerDo = do 
+    bool   <- _RANDOM_BOOL_
+    return $  getFirstPlayer bool
 
 -- Q#09
-
-getMoveDo = undefined
+-- Just did it with 'do' already :)
+getMoveDo :: Board -> IO Move
+getMoveDo b = getMove b
 
 -- Q#10
-
-playDo = undefined
+-- Just did it with 'do' already :)
+playDo :: Board -> IO ()
+playDo b = play b
